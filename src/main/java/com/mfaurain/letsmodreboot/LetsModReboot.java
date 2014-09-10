c4b45d141b97a884751dd94e70cbefcea30d257c
MFAurain <aurain@gmail.com>
2014-09-10 11:10:24 +0100

    Up to end of episode 7

--- src/main/java/com/mfaurain/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/mfaurain/letsmodreboot/LetsModReboot.java
@@ -1,17 +1,16 @@
 package com.mfaurain.letsmodreboot;
 
-import com.mfaurain.letsmodreboot.configuration.ConfigurationHandler;
+import com.mfaurain.letsmodreboot.handler.ConfigurationHandler;
 import com.mfaurain.letsmodreboot.proxy.IProxy;
 import com.mfaurain.letsmodreboot.reference.Reference;
+import com.mfaurain.letsmodreboot.utility.LogHelper;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-import javax.security.auth.callback.ConfirmationCallback;
-
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class LetsModReboot
 {
 
@@ -25,18 +24,19 @@ public class LetsModReboot
     public void preInit(FMLPreInitializationEvent event)
     {
         ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        LogHelper.info("Pre-initialisation Complete!");
     }
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-
+        LogHelper.info("Initialsation Complete!");
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-
+        LogHelper.info("Post-initialisation Complete!");
     }
 
 }
--- src/main/java/com/mfaurain/letsmodreboot/reference/Reference.java
+++ src/main/java/com/mfaurain/letsmodreboot/reference/Reference.java
@@ -4,8 +4,8 @@ public class Reference
 {
     public static final String MOD_ID = "LetsModReboot";
     public static final String MOD_NAME = "Lets Mod Reboot";
-    public static final String VERSION = "1.7.2-1.0";
+    public static final String VERSION = "1.7.10-1.0";
     public static final String CLIENT_PROXY_CLASS = "com.mfaurain.letsmodreboot.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.mfaurain.letsmodreboot.proxy.ServerProxy";
-
+    public static final String GUI_FACTORY_CLASS = "com.mfaurain.letsmodreboot.client.gui.GuiFactory";
 }
