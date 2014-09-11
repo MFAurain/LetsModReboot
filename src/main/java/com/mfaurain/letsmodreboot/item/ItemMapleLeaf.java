6ecb0f583db1b5757cfb9ea719479ca3e7e89b16
MFAurain <aurain@gmail.com>
2014-09-11 22:15:38 +0100

    Up to end of episode 8

---
+++ src/main/java/com/mfaurain/letsmodreboot/item/ItemMapleLeaf.java
@@ -0,0 +1,10 @@
+package com.mfaurain.letsmodreboot.item;
+
+public class ItemMapleLeaf extends ItemLMRB
+{
+    public ItemMapleLeaf()
+    {
+        super();
+        this.setUnlocalizedName("mapleLeaf");
+    }
+}
